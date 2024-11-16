6ab281b93604a39e63299ce94c5a524c530834c1
roysmanfo <manforoys@gmail.com>
2024-11-16 22:00:24 +0100

    update file to copy the jre into the build folder

--- build_files.py
+++ build_files.py
@@ -1,24 +1,64 @@
 import os
 import shutil
+from pathlib import Path
+import dotenv
+import subprocess
+import sys
 
 
 # copy assets from the src folder to the build folder
-root = "src/main/java/org/cup"
-assets = "src/main/java/org/cup/assets"
-exe_dir = "build/executable/main"
+curdir = Path(__file__).parent
+root = Path("src/main/java/org/cup")
+assets =  root / "assets"
+exe_dir = Path("build/executable/")
+main_dir = exe_dir / "main"
+
+NOT_SET = "..."
+
 
 def try_to_copy(src: str, dest: str):
     try:
         shutil.copytree(src, dest)
     except:
+        print("no: " , src)
         pass
 
-try_to_copy(os.path.join(assets, "UI", "fonts"), os.path.join(exe_dir, "UI", "fonts"))
-try_to_copy(os.path.join(assets, "audio"), os.path.join(exe_dir, "audio"))
-try_to_copy(os.path.join(assets, "sprites"), os.path.join(exe_dir, "sprites"))
+try_to_copy(assets / "UI" / "fonts", main_dir / "UI"/ "fonts")
+try_to_copy(assets / "audio", main_dir / "audio")
+try_to_copy(assets / "sprites", main_dir / "sprites")
 
 
 try:
-    shutil.copy(os.path.join(root, "logo.png"), os.path.join(exe_dir, "logo.png"))
+    shutil.copy(root / "logo.png", main_dir / "logo.png")
 except:
     pass
+
+
+# ==== JAVA STUFF =====
+
+# copy the jre to the build dir
+
+def find_jre_path() -> str | None:
+    if (envpath := curdir / ".env").exists():
+        dotenv.load_dotenv(envpath)
+    
+    if jre_home := os.getenv("JRE_HOME", default=None):
+        if jre_home != NOT_SET:
+            return jre_home
+    
+    # try to determine the path
+    if os.name == "nt":
+        jre_home = subprocess.run('powershell java -XshowSettings:properties -version 2>&1 | findstr "java.home"', text=True, capture_output=True)
+    else: # <- from wsl
+        jre_home = subprocess.check_output('java.exe -XshowSettings:properties -version 2>&1 | grep "java.home"', text=True)
+    
+    if jre_home is not None:
+        return jre_home.stdout.split("=")[-1].strip()
+
+JRE_HOME = find_jre_path()
+
+if not JRE_HOME:
+    print("[Err] unable to copy the jre")
+    sys.exit(1)
+
+try_to_copy(JRE_HOME, exe_dir / "jre")
